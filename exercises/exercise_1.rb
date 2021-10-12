78fa40e579bc5493d42f65df4e8cd502dc273744
Teenaelza <teena.thaliath@gmail.com>
2021-10-12 04:30:23 +0000

    Create records

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -2,5 +2,5 @@ require_relative '../setup'
 
 puts "Exercise 1"
 puts "----------"
+Store.create([{name:"Burnaby",annual_revenue:300000,mens_apparel:true,womens_apparel:true},{name:"Richmond",annual_revenue:1260000,mens_apparel:false,womens_apparel:true},{name:"Gastown",annual_revenue:190000,mens_apparel:true,womens_apparel:false}])
 
-# Your code goes below here ...
